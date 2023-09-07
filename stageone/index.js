729c4d10e9e35cf5d9d24c24ffeb59b74798c2f4
mryan-3 <malawaryan@gmail.com>
2023-09-07 19:09:54 +0300

    First Commit

---
+++ stageone/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const moment = require('moment')
+
+app.get('/api', (req, res) => {
+    const slack_name = req.query.slack_name
+    const current_day = moment().utc().format('dddd')
+    const utc_time = moment.utc().format('YYYY-MM-DDTHH:mm:ss\\Z')
+    const track = req.query.track
+    const github_file_url = ''
+    const github_repo_url = 'https://github.com/mryan-3/HNG.git'
+    const status_code = res.statusCode
+
+
+    const response = {
+        slack_name: slack_name,
+        current_day: current_day,
+        utc_time: utc_time,
+        track: track,
+        github_file_url: github_file_url,
+        github_repo_url: github_repo_url,
+        status_code: status_code
+    }
+
+    res.json(response)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
