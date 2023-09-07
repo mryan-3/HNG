c47764b6db512f7cc145f722c167ef2e5fe4a5bc
mryan-3 <113714055+mryan-3@users.noreply.github.com>
2023-09-07 21:09:12 +0300

    Update index.js

--- stageone/index.js
+++ stageone/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
+const now = new Date();
 const moment = require('moment')
 
 app.get('/api', (req, res) => {
     const slack_name = req.query.slack_name
     const current_day = moment().utc().format('dddd')
-    const utc_time = moment.utc().format('YYYY-MM-DDTHH:mm:ss\\Z')
+    const utc_time = now.toISOString().slice(0,19) + 'Z';
     const track = req.query.track
     const github_file_url = 'https://github.com/mryan-3/HNG/blob/main/stageone/index.js'
     const github_repo_url = 'https://github.com/mryan-3/HNG.git'
